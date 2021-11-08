6adc23aa263bc043e83c88a2c98e9f0dac811bd2
Kim-Rojas <kim.rojas@outlook.fr>
2021-11-08 22:50:42 +0100

    Ajout changement langue

--- BasketBallTracer/app/src/main/java/ck/edu/basketballtracer/MainActivity.java
+++ BasketBallTracer/app/src/main/java/ck/edu/basketballtracer/MainActivity.java
@@ -3,22 +3,39 @@ package ck.edu.basketballtracer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Spinner spinnerLanguage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
         connexionToDB();
+        String[] langues = new String[2];
+        langues[0] = "Francais";
+        langues[1] = "English";
+
+        this.spinnerLanguage = (Spinner) findViewById(R.id.spinnerLang);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item,
+                langues);
+
+        this.spinnerLanguage.setAdapter(adapter);
     }
 
 
@@ -52,5 +69,48 @@ public class MainActivity extends AppCompatActivity {
     public void redirectMap(View view){
         Intent monIntent = new Intent(MainActivity.this, MapsActivity.class);
         startActivity(monIntent);
+    }
+
+    public void getLanguage(View view){
+        Button bouton = (Button) findViewById(R.id.langue);
+        String lang;
+
+        Resources res = getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        Configuration conf = res.getConfiguration();
+
+        if(spinnerLanguage.getSelectedItem().equals("Francais")){
+            lang = "fr";
+            changeLanguage(lang);
+
+        } else {
+            lang = "en";
+            changeLanguage(lang);
+        }
+
+        //setButtonLanguage();
+    }
+
+    public void changeLanguage(String lang){
+        Locale myLocale = new Locale(lang);
+        Resources res = getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        Configuration conf = res.getConfiguration();
+        conf.locale = myLocale;
+        res.updateConfiguration(conf, dm);
+        Intent refresh = new Intent(this, MainActivity.class);
+        startActivity(refresh);
+
+        //finish();
+    }
+
+    public void setButtonLanguage(){
+        Button bouton = (Button) findViewById(R.id.langue);
+
+        if(bouton.equals("Francais") || bouton.equals("French")){
+            bouton.setText("English");
+        } else {
+            bouton.setText("Francais");
+        }
     }
 }
